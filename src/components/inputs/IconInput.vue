911aec11459228f9da4afc5295b02881e235223f
Léo DESIGAUX <leodesigaux@gmail.com>
2018-01-05 04:31:43 +0100

    Add Icon input component

---
+++ src/components/inputs/IconInput.vue
@@ -0,0 +1,28 @@
+<template>
+    <b-Icon :icon="customSizeByPack" :size="size"></b-Icon>
+</template>
+
+<script>
+  export default {
+    name: 'IconInput',
+    props: {
+      icon: String,
+      size: String
+    },
+    computed: {
+      customSizeByPack () {
+        switch (this.icon) {
+          case 'text': return 'view-headline'
+          case 'date': return 'calendar-clock'
+          case 'numeric': return 'numeric'
+          case 'location': return 'map-marker'
+          case 'image': return 'image'
+          case 'boolean': return 'toggle-switch-off'
+          case 'tags': return 'tag'
+          case 'sub': return 'animation'
+          default: return 'help'
+        }
+      }
+    }
+  }
+</script>
