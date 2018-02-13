fde2455013c6ae06b3afc179d842d8a67e44faf0
Léo DESIGAUX <leodesigaux@gmail.com>
2018-02-13 22:55:16 +0100

    Change usereditor to card design

--- src/components/ui/UserEditor.vue
+++ src/components/ui/UserEditor.vue
@@ -1,27 +1,24 @@
 <template>
 <form action="">
-    <div class="modal-card">
-      <header class="modal-card-head">
-         <p v-if="index === -1" class="modal-card-title">Nouvel utilisateur</p>
-         <p v-else class="modal-card-title">Édition de {{ user.name }}</p>
-        <button class="delete" aria-label="close" @click.prevent="$parent.close()"></button>
-      </header>
-      <section class="modal-card-body">
-        <div class="columns">
-          <div class="column">
-            <div class="avatar-container">
-              <img class="avatar" :src="'https://www.gravatar.com/avatar/' + user.avatar + '.jpg?s=256&avatar'" :alt="user.name + 'gravatar'">
+<transition appear name="fadeup">
+    <div class="card card--list">
+      <div class="card-image" :class="{'is-admin': user.role==='admin'}">
+        <figure class="image is-4by1">
+            <UserAvatar :user="user"/>
+            <div>
+              <a @click.prevent="$parent.close()">
+                <b-Icon icon="close"></b-Icon>
+              </a>
             </div>
-          </div>
-          <div class="column is-three-quarters">
-            <b-field label="Nom">
-                <b-input v-model="user.name" placeholder="Nom..."></b-input>
-            </b-field>
-            <b-field label="Email">
-                <b-input v-model="user.email" placeholder="Email..." type="email"></b-input>
-            </b-field>
-          </div>
-        </div>
+        </figure>
+      </div>
+      <div class="card-content">
+        <b-field label="Nom">
+            <b-input v-model="user.name" placeholder="Nom..."></b-input>
+        </b-field>
+        <b-field label="Email">
+            <b-input v-model="user.email" placeholder="Email..." type="email"></b-input>
+        </b-field>
         <b-field label="Role">
             <b-input placeholder="Role" disabled value="Utilisateur"></b-input>
         </b-field>
@@ -29,17 +26,15 @@
             <b-input type="password" v-model="user.newpassword" placeholder="Mot de passe..."></b-input>
         </b-field>
         <button v-show="!isPasswordActive && index !== -1" @click.prevent="isPasswordActive = true" class="button">Changer le mot de passe</button>
-      </section>
-      <footer class="modal-card-foot">
-        <button class="button is-primary" @click.prevent="save">
-          <b-icon icon="check"></b-icon>
-          <span>Valider</span>
-        </button>
-      </footer>
+        <button @click.prevent="save()" class="button is-info">Sauvegarder</button>
+      </div>
     </div>
+  </transition>
+
   </form>
 </template>
 <script>
+  import UserAvatar from '@/components/ui/UserAvatar'
   export default {
     name: 'UserEditor',
     props: ['user', 'index'],
@@ -49,6 +44,7 @@
       }
     },
     components: {
+      UserAvatar
     },
     methods: {
       save () {
@@ -70,6 +66,48 @@
   align-items:center;
   img{
     border-radius: 5%;
+  }
+}
+
+.card{
+  min-width: 400px;
+  border-radius: 10px;
+  overflow: hidden;
+  box-shadow: 0 2px 6px rgba(10, 10, 10, 0.1), 0 10px 6px 1px rgba(10, 10, 10, 0.03);
+  .card-image{
+    background-color: #8EC5FC;
+    background-image: linear-gradient(62deg, #8EC5FC 0%, #E0C3FC 100%);
+      position:relative;
+      .is-4by1{
+        padding-top:25%;
+        img{
+          position: absolute;
+          width: auto;
+          height: 120%;
+          left: 50%;
+          transform:translateX(-50%);
+          top: 30%;
+          border-radius: 50%;
+          background:white;
+          border: 5px solid white;
+          box-shadow:0 2px 5px rgba(0,0,0,.1);
+        }
+        div{
+          position: absolute;
+          top: 10px;
+          right: 10px;
+          & a .icon{
+            color:rgba(255,255,255,.6);
+            &:hover{
+              color:rgba(255,255,255,1);
+            }
+          }
+        }
+      }
+  }
+  .card-image.is-admin{
+    background-color: #FBDA61;
+    background-image: linear-gradient(45deg, #FBDA61 0%, #FF5ACD 100%);
   }
 }
 </style>
