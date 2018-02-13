44d07cc525583b7edb51bf2449f4d403add18eef
Léo DESIGAUX <leodesigaux@gmail.com>
2018-02-13 18:38:12 +0100

    [Feature] User avatars
    gravatar, fallback to svg

---
+++ src/components/ui/UserAvatar.vue
@@ -0,0 +1,26 @@
+<template>
+<img :src="fallback" alt="Avatar">
+</template>
+<script>
+export default {
+  name: 'UserAvatar',
+  props: ['size', 'user'],
+  data () {
+    return {
+      gravatar: 'https://www.gravatar.com/avatar/' + this.user.avatar + '.jpg?s=30&avatar',
+      fallback: '/src/assets/avatar-' + this.user.role + '.svg'
+    }
+  },
+  mounted () {
+    console.log('test')
+    let img = new Image()
+    img.onload = evt => {
+      this.fallback = this.gravatar
+    }
+    img.src = this.gravatar
+  }
+}
+</script>
+<style scoped lang="scss">
+
+</style>
