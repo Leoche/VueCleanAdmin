de4fd3d30d721263e3875834dc252e9b0d7fea1a
Léo DESIGAUX <leodesigaux@gmail.com>
2018-02-22 21:17:33 +0100

    Added Users store

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import account from './modules/account'
 import model from './modules/model'
+import users from './modules/users'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
     account: account,
-    model: model
+    model: model,
+    users: users
   }
 })
---
+++ src/store/modules/users.js
@@ -0,0 +1,133 @@
+import Vue from 'vue'
+
+const state = {
+  users: []
+}
+const actions = {
+  fetchUsers (state, user) {
+    let api = document.querySelector('meta[name=api]').content
+    return new Promise((resolve, reject) => {
+      return Vue.http.post(api, {
+        action: 'getusers',
+        email: user.email,
+        password: user.password
+      },
+        {
+          headers: {
+            'Content-Type': 'multipart/form-data'
+          },
+          emulateJSON: true
+        }
+      ).then(res => {
+        if (res.data.state === 'success') {
+          state.commit('SET_USERS', res.data.body)
+          resolve(res)
+        } else { reject(new Error('bad.credentials')) }
+      }, res => { reject(new Error('unreachable')) })
+    })
+  },
+  removeUser (state, payload) {
+    let api = document.querySelector('meta[name=api]').content
+    return new Promise((resolve, reject) => {
+      return Vue.http.post(api, {
+        action: 'removeuser',
+        email: payload.user.email,
+        password: payload.user.password,
+        useremail: state.getters.getUsers[payload.index].email
+      },
+        {
+          headers: {
+            'Content-Type': 'multipart/form-data'
+          },
+          emulateJSON: true
+        }
+      ).then(res => {
+        if (res.data.state === 'success') {
+          state.commit('REMOVE_USER', payload.index)
+          resolve(res)
+        } else { reject(new Error('bad.credentials')) }
+      }, res => { reject(new Error('unreachable')) })
+    })
+  },
+  addUser (state, payload) {
+    let api = document.querySelector('meta[name=api]').content
+    return new Promise((resolve, reject) => {
+      return Vue.http.post(api, {
+        action: 'adduser',
+        email: payload.user.email,
+        password: payload.user.password,
+        useremail: payload.newuser.email,
+        username: payload.newuser.name,
+        userpassword: payload.newuser.newpassword
+      },
+        {
+          headers: {
+            'Content-Type': 'multipart/form-data'
+          },
+          emulateJSON: true
+        }
+      ).then(res => {
+        if (res.data.state === 'success') {
+          state.commit('ADD_USER', {
+            email: payload.newuser.email,
+            name: payload.newuser.name,
+            role: 'user',
+            password: payload.newuser.newpassword
+          })
+          resolve(res)
+        } else { reject(new Error('bad.credentials')) }
+      }, res => { reject(new Error('unreachable')) })
+    })
+  },
+  editUser (state, payload) {
+    let api = document.querySelector('meta[name=api]').content
+    return new Promise((resolve, reject) => {
+      return Vue.http.post(api, {
+        action: 'edituser',
+        email: payload.user.email,
+        password: payload.user.password,
+        useroldemail: payload.newuser.oldemail,
+        useremail: payload.newuser.email,
+        username: payload.newuser.name,
+        userpassword: payload.newuser.password
+      },
+        {
+          headers: {
+            'Content-Type': 'multipart/form-data'
+          },
+          emulateJSON: true
+        }
+      ).then(res => {
+        if (res.data.state === 'success') {
+          state.commit('EDIT_USER', payload)
+          resolve(res)
+        } else { reject(new Error('bad.credentials')) }
+      }, res => { reject(new Error('unreachable')) })
+    })
+  }
+}
+const getters = {
+  getUsers: state => state.users
+}
+const mutations = {
+  SET_USERS (state, users) {
+    state.users = users
+  },
+  ADD_USER (state, user) {
+    state.users.push(user)
+  },
+  EDIT_USER (state, payload) {
+    state.users[payload.index].name = payload.newuser.name
+    state.users[payload.index].email = payload.newuser.email
+  },
+  REMOVE_USER (state, index) {
+    state.users.splice(index, 1)
+  }
+}
+
+export default {
+  state,
+  getters,
+  actions,
+  mutations
+}
