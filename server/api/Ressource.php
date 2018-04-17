1f37692cf217f13eb48b1a7a8f404c77da645fbf
Léo DESIGAUX <leodesigaux@gmail.com>
2018-04-17 16:29:30 +0200

    Better server structure

--- server/api/Ressource.php
+++ server/api/Ressource.php
@@ -1,9 +1,9 @@
 <?php
 class Ressource{
    public function getJSON($filename) {
-      $json = @file_get_contents("../".$filename.".json");
+      $json = @file_get_contents("../storage/config/".$filename.".json");
       if ($json === FALSE){
-        if(file_put_contents("../".$filename.".json", "{}")) {
+        if(file_put_contents("../storage/config/".$filename.".json", "{}")) {
           return "{}";
         } throw new Exception("Can't access to the ".$filename.".json, can't create it either (wrong permissions)");
       }
@@ -14,7 +14,7 @@ class Ressource{
          $body = json_encode($body);
       $json = json_decode($body);
       if (json_last_error() !== JSON_ERROR_NONE)  throw new Exception("Can't save a non JSON file");
-      if (@file_put_contents("../".$filename.".json", $body) === FALSE) throw new Exception("Can't save to ".$filename.".json");
+      if (@file_put_contents("../storage/config/".$filename.".json", $body) === FALSE) throw new Exception("Can't save to ".$filename.".json");
       else return true;
    }
    public function upload($filename, $tmp_file) {
