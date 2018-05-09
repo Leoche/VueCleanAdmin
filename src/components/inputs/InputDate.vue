ffca483f19e08483435633aa4da4b07da0b7d28d
Léo DESIGAUX <leodesigaux@gmail.com>
2018-05-09 15:24:30 +0200

    Added support for input date

---
+++ src/components/inputs/InputDate.vue
@@ -0,0 +1,29 @@
+<template>
+ <section class="content">
+  <b-field :label="label">
+        <b-datepicker
+            v-model="value"
+            placeholder="Ajouter une Date"
+            icon="calendar-today"
+            @input="change">
+        </b-datepicker>
+  </b-field>
+</section>
+</template>
+
+<script>
+import InputBase from '@/components/inputs/InputBase'
+
+export default {
+  name: 'InputDate',
+  extends: InputBase,
+  data () {
+    return {
+      value: new Date()
+    }
+  },
+  mounted () {
+    this.value = new Date(this.defaultvalue)
+  }
+}
+</script>
