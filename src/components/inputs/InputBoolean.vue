cda0ef4d3683601feefff8d778f8958f4178cbb6
Léo DESIGAUX <leodesigaux@gmail.com>
2018-05-17 18:57:12 +0200

    Fixed double to single quote for eslint

--- src/components/inputs/InputBoolean.vue
+++ src/components/inputs/InputBoolean.vue
@@ -27,9 +27,9 @@ export default {
   computed: {
     actived () {
       if (this.value) {
-        return "Activé"
+        return 'Activé'
       } else {
-        return "Désactivé"
+        return 'Désactivé'
       }
     }
   }
